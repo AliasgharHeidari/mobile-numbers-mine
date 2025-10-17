2aa76e37f7fcbeb0a738367d375dfd573d8a6ebd
AliasgharHeidari <aliasgharaliali1213@gmail.com>
2025-10-17 13:04:30 +0330

    feat : added jwt authentication

---
+++ internal/api/handler/auth.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"time"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func Login(c *fiber.Ctx) error {
+	type request struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	var body request
+	err := c.BodyParser(&body)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid request body",
+		})
+	}
+	 
+	if body.Username != "Aliasghar" || body.Password != "1234" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error":"wrong credientials",
+		})
+	}
+
+	claims := jwt.MapClaims{
+		"username" : body.Username,
+		"exp" : time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to create token",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"token": tokenString,
+	})
+}
+
