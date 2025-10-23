258095465ff48cfac9bb58a523bf604791a5b44c
AliasgharHeidari <aliasgharaliali1213@gmail.com>
2025-10-23 20:25:11 +0330

    fix : fixed typo

--- internal/config/api.go
+++ internal/config/api.go
@@ -1,20 +1,20 @@
-package config
+package config 
 
 type API struct {
-	Server APIServerConfig `yaml:"server"`
-	JWT    JWTConfig       `yaml:"jwt"`
+	Server APIServerConfig `yaml:"server"` 
+	JWT    JWTConfig       `yaml:"jwt"` 
 }
 
 type APIServerConfig struct {
-	ListenAddress string `yaml:"listen_address"`
-	ListenPort    string `yaml:"listen_port"`
+	ListenAddress string `yaml:"listen_address"` 
+	ListenPort    string `yaml:"listen_port"` 
 }
 
 func (a *APIServerConfig) ListenString() string {
-	return a.ListenAddress + ":" + a.ListenPort
+	return a.ListenAddress + ":" + a.ListenPort 
 }
 
 type JWTConfig struct {
 	SecretKey     string `yaml:"secret_key"`
-	TokenDuration int    `yaml:"expiration_minutes"` // in minutes
+	TokenDuration int    `yaml:"expiration_minutes"` // in minutes 
 }
