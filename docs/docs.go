fe51e2e55f185850da785d1337a8fc0263fed6f0
Mahmoud Ahmadi <ma.ahmadi1989@gmail.com>
2025-10-31 14:19:38 +0330

    docs: updated the swagger doc main info

--- docs/docs.go
+++ docs/docs.go
@@ -484,7 +484,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "http://[host-ip]:[host-port]/swagger/index.html",
+	Host:             "http://127.0.0.1:9898/swagger/index.html",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Mine API - Swagger Docs",
