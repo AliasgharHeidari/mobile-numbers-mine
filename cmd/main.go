04cb2bf9e2363ff11b4380f6572600cff81642cd
Mahmoud Ahmadi <ma.ahmadi1989@gmail.com>
2025-10-31 13:56:34 +0330

    doc: updated the swagger header info

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,14 @@
-// @title Mobile Numbers Mine API
+// @title Mine API - Swagger Docs
 // @version 1.0
 // @description API for managing users and their mobile numbers.
-// @contact.name Aliasghar
-// @host localhost:9898
+// @contact.name Aliasghar Heidari
+// @contact.email ali.heidari@gmail.com
+// @host http://[host-ip]:[host-port]/swagger/index.html
 // @BasePath /
-// @securityDefinitions.apikey ApiKeyAuth
+// @securityDefinitions.apikey BearerAuth
 // @in header
 // @name Authorization
+// @description Type "Bearer" followed by a space and JWT token.
 package main
 
 import (
