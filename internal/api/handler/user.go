daccb7f7cb483b5e3fa19948824ecbae3838e232
AliasgharHeidari <aliasgharaliali1213@gmail.com>
2025-10-31 13:03:36 +0330

    feat : added swagger comments

--- internal/api/handler/user.go
+++ internal/api/handler/user.go
@@ -8,6 +8,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// GetUserList godoc
+// @Summary List users
+// @Tags Users
+// @Description Get list of users
+// @Produce json
+// @Success 200 {array} object "users"
+// @Failure 401 {object} map[string]interface{} "unauthorized"
+// @Security ApiKeyAuth
+// @Router /user [get]
 func GetUserList(c *fiber.Ctx) error {
 	usersList, err := service.GetUserList()
 	if err != nil {
@@ -18,6 +27,18 @@ func GetUserList(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(usersList)
 }
 
+// CreateUser godoc
+// @Summary Create user
+// @Tags Users
+// @Description Create a new user
+// @Accept json
+// @Produce json
+// @Param user body object true "user payload"
+// @Success 201 {object} object "created user"
+// @Failure 400 {object} map[string]interface{} "bad request"
+// @Failure 401 {object} map[string]interface{} "unauthorized"
+// @Security ApiKeyAuth
+// @Router /user [post]
 func CreateUser(c *fiber.Ctx) error {
 	var newUser model.User
 	if err := c.BodyParser(&newUser); err != nil {
@@ -39,6 +60,17 @@ func CreateUser(c *fiber.Ctx) error {
 	})
 }
 
+// GetUserByID godoc
+// @Summary Get user by ID
+// @Tags Users
+// @Description Get user by ID
+// @Produce json
+// @Param id path string true "User ID"
+// @Success 200 {object} object "user"
+// @Failure 401 {object} map[string]interface{} "unauthorized"
+// @Failure 404 {object} map[string]interface{} "not found"
+// @Security ApiKeyAuth
+// @Router /user/{id} [get]
 func GetUserByID(c *fiber.Ctx) error {
 	userID := c.Params("id")
 	id, err := strconv.Atoi(userID)
@@ -57,7 +89,20 @@ func GetUserByID(c *fiber.Ctx) error {
 
 	return c.Status(fiber.StatusOK).JSON(user)
 }
-
+// UpdateUserByID godoc
+// @Summary Update user
+// @Tags Users
+// @Description Update user by ID
+// @Accept json
+// @Produce json
+// @Param id path string true "User ID"
+// @Param user body object true "user payload"
+// @Success 200 {object} object "updated user"
+// @Failure 400 {object} map[string]interface{} "bad request"
+// @Failure 401 {object} map[string]interface{} "unauthorized"
+// @Failure 404 {object} map[string]interface{} "not found"
+// @Security ApiKeyAuth
+// @Router /user/{id} [put]
 func UpdateUserByID(c *fiber.Ctx) error {
 	userID := c.Params("id")
 	id, err := strconv.Atoi(userID)
@@ -85,6 +130,16 @@ func UpdateUserByID(c *fiber.Ctx) error {
 	})
 }
 
+// DeleteUserByID godoc
+// @Summary Delete user
+// @Tags Users
+// @Description Delete user by ID
+// @Param id path string true "User ID"
+// @Success 204 {string} string "no content"
+// @Failure 401 {object} map[string]interface{} "unauthorized"
+// @Failure 404 {object} map[string]interface{} "not found"
+// @Security ApiKeyAuth
+// @Router /user/{id} [delete]
 func DeleteUserByID(c *fiber.Ctx) error {
 	userID := c.Params("id")
 	id, err := strconv.Atoi(userID)
@@ -105,6 +160,19 @@ func DeleteUserByID(c *fiber.Ctx) error {
 
 }
 
+// AddMobileNumber godoc
+// @Summary Add mobile number
+// @Tags MobileNumbers
+// @Description Add a mobile number to user
+// @Accept json
+// @Produce json
+// @Param id path string true "User ID"
+// @Param number body object true "mobile number payload"
+// @Success 201 {object} object "added number"
+// @Failure 400 {object} map[string]interface{} "bad request"
+// @Failure 401 {object} map[string]interface{} "unauthorized"
+// @Security ApiKeyAuth
+// @Router /user/{id}/mobile-number [post]
 func AddMobileNumber(c *fiber.Ctx) error {
 	userID := c.Params("id")
 	id, err := strconv.Atoi(userID)
@@ -130,7 +198,17 @@ func AddMobileNumber(c *fiber.Ctx) error {
 		"message" : "Mobile Number addded successfully",
    })
 }
-
+// DeleteMobileNumber godoc
+// @Summary Delete mobile number
+// @Tags MobileNumbers
+// @Description Delete a mobile number of a user
+// @Param id path string true "User ID"
+// @Param number path string true "mobile number"
+// @Success 204 {string} string "no content"
+// @Failure 401 {object} map[string]interface{} "unauthorized"
+// @Failure 404 {object} map[string]interface{} "not found"
+// @Security ApiKeyAuth
+// @Router /user/{id}/mobile-number/{number} [delete]
 func DeleteMobileNumber(c *fiber.Ctx) error {
 	userID := c.Params("id")
 	id , err := strconv.Atoi(userID)
